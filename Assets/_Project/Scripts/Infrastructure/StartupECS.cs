f1e343de710b8c407cfb806f220dd5ef58c73229
Andrei Savchenko <guiltyvlr4u@gmail.com>
2025-08-31 00:47:23 +1000

    feat: creating and initializing new systems

--- Assets/_Project/Scripts/Infrastructure/StartupECS.cs
+++ Assets/_Project/Scripts/Infrastructure/StartupECS.cs
@@ -1,25 +1,36 @@
+using _Project.Scripts.BusinessContainer;
+using _Project.Scripts.BusinessContainer.View;
 using _Project.Scripts.BusinessTotalAmount;
 using _Project.Scripts.BusinessTotalAmount.View;
+using _Project.Scripts.Configs;
 using _Project.Scripts.Infrastructure.Facades;
 using Scellecs.Morpeh;
 using UnityEngine;
 
 namespace _Project.Scripts.Infrastructure
 {
-    public class StartupECS : MonoBehaviour
+    public class StartupEcs : MonoBehaviour
     {
         [SerializeField] private BaseBusinessWindowView businessWindowView;
-        
+        [SerializeField] private GlobalBusinessesConfig _config;
+
+        [SerializeField] private BaseBusinessContainerView _viewPrefab;
+        [SerializeField] private Transform _bussinessesParent;
+
         private World _world;
 
         public void Start()
         {
             _world = World.Default;
 
-            var totalMoneyAmountSystemGroup = _world.CreateSystemsGroup();
-            totalMoneyAmountSystemGroup.AddSystem(new TotalMoneySystem(new DefaultUIFacade(businessWindowView)));
+            var moneyCounterGroup = _world.CreateSystemsGroup();
+            moneyCounterGroup.AddSystem(new TotalMoneyCounter(new DefaultUIFacade(businessWindowView)));
+            moneyCounterGroup.AddSystem(new IncomeCounter());
+            moneyCounterGroup.AddInitializer(new BusinessesInitialize(_config,
+                new BusinessContainerFactory(_viewPrefab, _bussinessesParent)));
 
-            _world.AddSystemsGroup(0, totalMoneyAmountSystemGroup);
+
+            _world.AddSystemsGroup(0, moneyCounterGroup);
         }
     }
 }
