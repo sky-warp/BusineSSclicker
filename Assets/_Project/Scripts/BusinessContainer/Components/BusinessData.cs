09711f152b3094ef64cf6230f199e580ad59dacd
Andrei Savchenko <guiltyvlr4u@gmail.com>
2025-08-31 23:44:55 +1000

    feat: remote all business text to specified component

---
+++ Assets/_Project/Scripts/BusinessContainer/Components/BusinessData.cs
@@ -0,0 +1,13 @@
+using Scellecs.Morpeh;
+
+namespace _Project.Scripts.BusinessContainer.Components
+{
+    public struct BusinessData : IComponent
+    {
+        public int CurrentLevel;
+        public float Income;
+        public float Delay;
+        public float Timer;
+        public float IncomeProgressBarValue;
+    }
+}
---
+++ Assets/_Project/Scripts/BusinessContainer/Components/BusinessText.cs
@@ -0,0 +1,12 @@
+using Scellecs.Morpeh;
+
+namespace _Project.Scripts.BusinessContainer.Components
+{
+    public struct BusinessText : IComponent
+    {
+        public string Name;
+        public string LevelUpText;
+        public string Upgrade1Text;
+        public string Upgrade2Text;
+    }
+}
